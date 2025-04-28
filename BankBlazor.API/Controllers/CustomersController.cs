19d15a4fcc67173f2e47f28c4a18a3615e7bc052
Adnan Jamil <jamil.adnan@live.com>
2025-04-29 01:02:42 +0200

    Created Customer controller and GET method to fetch all the customers . and also a specific customer by customer id.

---
+++ BankBlazor.API/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using BankBlazor.API.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankBlazor.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly BankBlazorContext _context;
+
+        public CustomersController(BankBlazorContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/customers  It is goin to get all the customers from the database
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            var customers = await _context.Customers.ToListAsync();
+            return Ok(customers);
+        }
+
+        // GET: api/customers/{id}  will fetch a specific customer with the id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomerById(int id)
+        {
+            var customer = await _context.Customers
+                                         .FirstOrDefaultAsync(c => c.CustomerId == id);
+
+            if (customer == null)
+            {
+                return NotFound(); // 404 if customer not found
+            }
+
+            return Ok(customer);
+        }
+    }
+}
