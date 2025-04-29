bcc57dee4ffd3722299f290e3a9d13c59a32f2bb
Adnan Jamil <jamil.adnan@live.com>
2025-04-29 22:31:20 +0200

    Updated the get methods to fetch only relevant data.

--- BankBlazor.API/Controllers/CustomersController.cs
+++ BankBlazor.API/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using BankBlazor.API.Data;
+using BankBlazor.API.DTO;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -18,26 +19,49 @@ namespace BankBlazor.API.Controllers
         }
 
         // GET: api/customers  It is goin to get all the customers from the database
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        [HttpGet("accounts")]
+        public async Task<ActionResult<IEnumerable<CustomerAccountAllDto>>> GetAllCustomersWithAccounts()
         {
-            var customers = await _context.Customers.ToListAsync();
-            return Ok(customers);
+            var customerAccounts = await _context.Dispositions
+                .Include(d => d.Customer)
+                .Include(d => d.Account)
+                .Where(d => d.Account != null && d.Customer != null)
+                .Select(d => new CustomerAccountAllDto
+                {
+                    FullName = d.Customer.Givenname + " " + d.Customer.Surname,
+                    AccountId = d.Account.AccountId,
+                    Balance = d.Account.Balance
+                })
+                .ToListAsync();
+
+            return Ok(customerAccounts);
         }
 
         // GET: api/customers/{id}  will fetch a specific customer with the id
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Customer>> GetCustomerById(int id)
+        [HttpGet("{id}/accounts")]
+        public async Task<ActionResult<CustomerAccountInfoDto>> GetCustomerWithAccounts(int id)
         {
             var customer = await _context.Customers
-                                         .FirstOrDefaultAsync(c => c.CustomerId == id);
+                .Include(c => c.Dispositions)
+                    .ThenInclude(d => d.Account)
+                .FirstOrDefaultAsync(c => c.CustomerId == id);
 
             if (customer == null)
-            {
-                return NotFound(); // 404 if customer not found
-            }
+                return NotFound();
 
-            return Ok(customer);
+            var dto = new CustomerAccountInfoDto
+            {
+                FullName = $"{customer.Givenname} {customer.Surname}",
+                Accounts = customer.Dispositions
+                    .Where(d => d.Account != null)
+                    .Select(d => new AccountDto
+                    {
+                        AccountId = d.Account.AccountId,
+                        Balance = d.Account.Balance
+                    }).ToList()
+            };
+
+            return Ok(dto);
         }
     }
 }
---
+++ BankBlazor.API/DTO/CustomerAccountInfoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BankBlazor.API.DTO
+{
+    public class CustomerAccountInfoDto
+    {
+        public string FullName { get; set; }
+        public List<AccountDto> Accounts { get; set; }
+    }
+
+    public class AccountDto
+    {
+        public int AccountId { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
