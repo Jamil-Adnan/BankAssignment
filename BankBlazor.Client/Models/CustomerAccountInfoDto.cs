3ba0b856392f8c38262f57e7f90200db48c150e2
Adnan Jamil <jamil.adnan@live.com>
2025-04-30 10:59:51 +0200

    need to work on Blazor. Api is working (Swagger shows related info) but blazor is not showing relevant data.

---
+++ BankBlazor.Client/Models/CustomerAccountInfoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BankBlazor.Client.Models
+{
+    public class CustomerAccountInfoDto
+    {
+        public string FullName { get; set; }
+        public List<AccountDto> Accounts { get; set; }
+    }
+
+    public class AccountDto
+    {
+        public int AccountId { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
