3ad0923d151f698957c5b65ebb3186006968ffd6
Adnan Jamil <jamil.adnan@live.com>
2025-05-01 02:08:30 +0200

    I dont know what to do!!!

---
+++ BankBlazor.Client/Models/DepositWithdrawRequestDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankBlazor.Client.Models
+{
+    public class DepositWithdrawRequestDto
+    {
+        [Required]
+        public int AccountId { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be greater than 0")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [RegularExpression("deposit|withdraw", ErrorMessage = "Must be 'deposit' or 'withdraw'")]
+        public string Operation { get; set; }
+
+        public string? Symbol { get; set; }
+    }
+}
