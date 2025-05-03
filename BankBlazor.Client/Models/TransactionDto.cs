7ca654d171425bc4f0c6b8c2a83b80e9b6f24a64
Adnan Jamil <jamil.adnan@live.com>
2025-05-04 01:25:36 +0200

    everything is working at last.

--- BankBlazor.Client/Models/TransactionDto.cs
+++ BankBlazor.Client/Models/TransactionDto.cs
@@ -2,9 +2,15 @@
 {
     public class TransactionDto
     {
-        public DateTime Date { get; set; }
+        public int TransactionId { get; set; }
+        public int AccountId { get; set; }
+        public DateOnly Date { get; set; }
+        public string Type { get; set; }
         public string Operation { get; set; }
         public decimal Amount { get; set; }
         public decimal Balance { get; set; }
+        public string Symbol { get; set; }
+        public string Bank { get; set; }
+        public string Account { get; set; }
     }
 }
