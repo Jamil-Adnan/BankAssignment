21e06e3267b34d9f3880bb360882e47c8905d32f
Adnan Jamil <jamil.adnan@live.com>
2025-05-03 10:47:15 +0200

    The first page is working. Show all customers info with pagination.

--- BankBlazor.Client/Program.cs
+++ BankBlazor.Client/Program.cs
@@ -11,6 +11,7 @@ public class Program
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
+        //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7219/") });
 
         await builder.Build().RunAsync();
